1e38d26dbe14fd4d5ca052bc273a846b582fdb68
Brandon <brandis.vanegas2018@gmail.com>
2026-09-18 00:50:36 -0500

    Se agregan las consultas faltantes

--- Trimestre IV/DB SQL/consultas y subconsultas.sql
+++ Trimestre IV/DB SQL/consultas y subconsultas.sql
@@ -54,6 +54,7 @@ on d.usuario_idusuario = u.idusuario
 left join telefono t   
 on t.usuario_idusuario = u.idusuario
 order by c.id_cliente;
+
 -- - -- - - -- - -- - - -- - -- - -- --- -- - - - -- - - ---- -- - -
 -- consulta 5
 -- insumos por debajo del stock mínimo
@@ -64,6 +65,104 @@ on i.idinsumo = inv.insumo_idinsumo
 where inv.stock_actual <= inv.stock_minimo
 order by deficit desc;
 
+-- Consulta 6
+
+-- conocer cuánto ha comprado cada cliente, cuántas compras ha realizado y cuál ha sido su gasto promedio, discriminando el método de pago.
+SELECT CONCAT(u.primerNombre, ' ', COALESCE( u.segundoNombre,''),' ', u.primerApellido, ' ', 
+		COALESCE( u.segundoApellido, '')) AS nombreCliente,
+		COUNT(p.Cliente_id_cliente) AS cantidadCompras, mp.metodo,
+        SUM(v.total) AS totalGastado,
+		AVG(v.total) AS promedio
+        FROM usuario u INNER JOIN cliente c
+        ON u.idUsuario = c.Usuario_idUsuario
+        INNER JOIN pedido p
+        ON c.id_cliente = p.Cliente_id_cliente
+        INNER JOIN venta v
+        ON v.Pedido_idPedido= p.idPedido
+        INNER JOIN metodopago mp
+		ON mp.idMetodo_Pago = v.Metodo_Pago_idMetodo_Pago
+        GROUP BY u.idusuario, mp.metodo;
+        
+        
+-- Consulta 7
+
+-- Identificar qué platos tienen mayor demanda y cuánto dinero generan.
+SELECT
+    tp.nombre_tipo AS categoria,
+    pl.nombre AS plato,
+    COUNT(DISTINCT p.idPedido) AS pedidos_realizados,
+    SUM(dp.cantidad) AS unidades_vendidas,
+    SUM(dp.cantidad * dp.precio_unitario) AS ingresos_generados,
+    AVG(dp.precio_unitario) AS precio_promedio,
+    CASE
+        WHEN SUM(dp.cantidad) >= 50 THEN 'Alta demanda'
+        WHEN SUM(dp.cantidad) >= 20 THEN 'Demanda media'
+        ELSE 'Baja demanda'
+    END AS nivel_demanda
+FROM detalle_pedido dp
+INNER JOIN plato pl ON dp.Plato_idPlato = pl.idPlato
+INNER JOIN tipo_plato tp ON pl.Tipo_Plato_idTipo_Plato = tp.idTipo_Plato
+INNER JOIN pedido p ON dp.Pedido_idPedido = p.idPedido
+INNER JOIN estado_pedido ep ON p.Estado_Pedido_idEstado_Pedido = ep.idEstado_Pedido
+WHERE p.fecha_hora >= '2026-01-01'
+  AND ep.nombre_estado IN ('Entregado', 'Completado')
+GROUP BY pl.idPlato, pl.nombre, tp.nombre_tipo
+HAVING SUM(dp.cantidad) >= 10
+ORDER BY unidades_vendidas DESC;
+
+
+-- Consulta 8
+-- Cliente con 3 pedidos  o mas y su total
+	SELECT CONCAT(u.primerNombre, ' ', COALESCE( u.segundoNombre,''),' ', u.primerApellido, ' ', 
+		COALESCE( u.segundoApellido, '')) AS nombreCliente, COUNT(p.idPedido) AS cantidad_pedidos,
+    SUM(v.total) AS total_gastado, MAX(v.fecha) AS ultima_compra
+FROM cliente c INNER JOIN usuario u ON c.Usuario_idUsuario = u.idUsuario
+INNER JOIN pedido p ON c.id_cliente = p.Cliente_id_cliente
+INNER JOIN venta v ON p.idPedido = v.Pedido_idPedido
+GROUP BY c.id_cliente, u.primerNombre, u.segundoNombre, u.primerApellido
+HAVING COUNT(p.idPedido) >= 3
+ORDER BY cantidad_pedidos DESC;
+        
+        
+-- Consulta 9
+-- Identificar el estado de los insumos para verificar los que estan pronto a agotarse o que se llegaron agotar.
+SELECT
+    i.nombre AS Insumo, i.unidad_medida, inv.stock_actual,inv.stock_minimo,
+    CASE
+        WHEN inv.stock_actual = 0 THEN 'AGOTADO'
+        WHEN inv.stock_actual < inv.stock_minimo THEN 'CRITICO'
+        ELSE 'NORMAL'
+    END AS estado_stock,
+
+    COUNT(ai.idAlerta) AS cantidad_alertas, MAX(ai.fecha) AS ultima_alerta, CONCAT(u.primerNombre, ' ', u.primerApellido) AS administrador
+
+FROM inventario inv INNER JOIN insumo i ON inv.Insumo_idInsumo = i.idInsumo
+	LEFT JOIN alerta_inventario ai ON i.idInsumo = ai.Insumo_idInsumo
+INNER JOIN administrador a ON inv.Administrador_id_administrador = a.id_administrador
+INNER JOIN usuario u ON a.Usuario_idUsuario = u.idUsuario
+
+GROUP BY i.idInsumo, i.nombre, i.unidad_medida, inv.stock_actual, inv.stock_minimo, u.primerNombre, u.primerApellido
+
+ORDER BY inv.stock_actual ASC;
+
+
+-- Consulta 10
+
+-- Saber cuantos pedidos se han hecho segun la modalidadPedido
+SELECT
+    mp.nombreModalidad AS modalidad,
+    COUNT(p.idPedido) AS cantidad_pedidos,
+    SUM(v.total) AS total_ventas,
+    AVG(v.total) AS promedio_venta
+FROM pedido p
+INNER JOIN venta v
+    ON p.idPedido = v.Pedido_idPedido
+INNER JOIN modalidadpedido mp
+    ON p.modalidadPedido_id_modalidad = mp.id_modalidad
+INNER JOIN cliente c
+    ON p.Cliente_id_cliente = c.id_cliente
+GROUP BY mp.nombreModalidad
+ORDER BY total_ventas DESC;
 -- - -- - - -- - -- - - -- - -- - -- --- -- - - - -- - - ---- -- - -
 -- - -- - - -- - -- - - -- - -- - -- --- -- - - - -- - - ---- -- - -
 
@@ -111,3 +210,51 @@ select p.idpedido, p.mesa_idmesa, p.fecha_hora
 from pedido p
 where (p.mesa_idmesa, p.fecha_hora) in (select mesa_idmesa, max(fecha_hora) from pedido
 group by mesa_idmesa);
+
+
+-- subconsulta 6
+
+--  identificar los pedidos cuyo tiempo de preparación sea superior al promedio de tiempo de preparación de todos los pedidos.
+-- Para cada pedido quiere conocer el cliente, el mesero, la mesa y el tiempo de preparación.
+
+SELECT
+    pe.idPedido, CONCAT(um.primerNombre, ' ', COALESCE(um.segundoNombre, ''), ' ', um.primerApellido, ' ',
+        COALESCE(um.segundoApellido, '')
+    ) AS nombreMesero,
+
+    CONCAT( uc.primerNombre, ' ', COALESCE(uc.segundoNombre, ''), ' ', uc.primerApellido, ' ',
+        COALESCE(uc.segundoApellido, '')
+    ) AS nombreCliente,
+
+    mesa.numero_mesa, pe.tiempo_preparacion
+
+FROM Pedido pe INNER JOIN Cliente c 
+    ON pe.Cliente_id_cliente = c.id_cliente
+INNER JOIN Usuario uc 
+    ON c.Usuario_idUsuario = uc.idUsuario
+INNER JOIN Mesero me 
+    ON pe.Mesero_id_mesero = me.id_mesero
+INNER JOIN Usuario um 
+    ON me.Usuario_idUsuario = um.idUsuario
+INNER JOIN Mesa mesa 
+    ON pe.Mesa_idMesa = mesa.idMesa
+WHERE pe.tiempo_preparacion > ( SELECT AVG(tiempo_preparacion) FROM Pedido);
+
+
+-- Subconsulta 7
+-- detectar insumos que tienen una cantidad disponible inferior al promedio general del inventario.
+
+SELECT i.nombre AS insumo, i.unidad_medida, inv.stock_actual, inv.stock_minimo,
+    a.mensaje AS ultima_alerta, a.fecha AS fecha_alerta
+FROM inventario inv INNER JOIN insumo i
+    ON inv.Insumo_idInsumo = i.idInsumo
+LEFT JOIN alerta_inventario a
+    ON i.idInsumo = a.Insumo_idInsumo
+INNER JOIN administrador adm
+    ON inv.Administrador_id_administrador = adm.id_administrador
+INNER JOIN usuario u
+    ON adm.Usuario_idUsuario = u.idUsuario
+
+WHERE inv.stock_actual < ( SELECT AVG(stock_actual) FROM inventario) AND inv.stock_actual <= inv.stock_minimo
+ORDER BY inv.stock_actual ASC;
+
