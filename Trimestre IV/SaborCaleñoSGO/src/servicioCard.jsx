2624c153cd9bbf1807b77c0db61f09a61bc858e9
Alexandra193-arch <alexandra.rodriguezleguizamon@gmail.com>
2026-09-23 16:04:22 -0500

    Actualización interfaz Sabor Caleño

---
+++ Trimestre IV/SaborCaleñoSGO/src/servicioCard.jsx
@@ -0,0 +1,11 @@
+function sercioCard ({imagen, titulo, descripcion}){
+    return(
+        <div className="card h-100 shadow-sm">
+            <img src={imagen}
+            className="card-img-top"
+            />
+            
+        
+        </div>
+    )
+}
